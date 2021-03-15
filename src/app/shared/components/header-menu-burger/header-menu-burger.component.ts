05d77e51b2bee13f19fe9c03537bca7dec4c2ae5
tengizmikhelidze <mixelidzetengizi@gmail.com>
2021-03-15 14:13:45 +0400

    fix - responsive menu

--- src/app/shared/components/header-menu-burger/header-menu-burger.component.ts
+++ src/app/shared/components/header-menu-burger/header-menu-burger.component.ts
@@ -1,5 +1,7 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { FirebaseService } from '../../services/firebase.service';
 
 @Component({
   selector: 'app-header-menu-burger',
@@ -10,7 +12,7 @@ import { Component, OnInit } from '@angular/core';
       state('open', style({
         width: '50%',
         height: '100vh',
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
       })),
       state('closed', style({
         width: '0px',
@@ -20,13 +22,21 @@ import { Component, OnInit } from '@angular/core';
         color: '#fff'
       })),
       state('openBurger', style({
-        color: '000'
+        color: '#000'
+      })),
+      state('openContent', style({
+        opacity: '1',
+        width: '100%',
+      })),
+      state('closedContent', style({
+        opacity: '0',
+        width: '0%',
       })),
       transition('open <=> closed',[
-        animate('0.5s')
+        animate(300)
       ]),
       transition('openBurger <=> closedBurger',[
-        animate('0.5s')
+        animate(300)
       ])
     ])
   ]
@@ -34,18 +44,41 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderMenuBurgerComponent implements OnInit {
   animationState: "closed" | "open" = 'closed';
   burgerState: "closedBurger" | "openBurger" = "closedBurger";
-  constructor() { }
+  contentState: "closedContent" | "openContent" = "closedContent";
+  userName: string;
+  userSurname: string;
+  constructor(
+    private firebaseService: FirebaseService
+  ) { }
 
   ngOnInit(): void {
+    if(this.firebaseService.isLoggedIn){
+      this.firebaseService.searchUser(this.firebaseService.user.uid).pipe(
+        map((user)=>{
+          this.userSurname = user.surname;
+          this.userName = user.name;
+        })
+      ).subscribe()
+    }
   }
 
   openMenu(){
     if(this.animationState === "closed"){
       this.animationState = "open";
       this.burgerState = "openBurger"
+      this.contentState = 'openContent';
     } else {
       this.animationState = "closed";
       this.burgerState = "closedBurger";
+      this.contentState = "closedContent";
     }
+  }
+  routerClick(event){
+    var deactive = document.getElementsByClassName('active')[1];
+    deactive.className = 'inactive';
+    event.target.className = 'active';
+  }
+  logout(){
+
   }
 }
